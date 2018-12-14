85d814ff68e10cab49d3559ad274627e4e74a1e5
glaracuente <glaracuente@outbrain.com>
2018-12-14 00:12:19 -0500

    fixing schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,7 +3,10 @@ VALUES ('Gerry'),
 ('Chris'), 
 ('Will');
 
-INSERT INTO events (userid, title, from_date, to_date)
-VALUES (1, 'bowling night', '2018-12-10', '2018-12-20'), 
-(2, 'game night', '2018-12-10', '2018-12-20'), 
-(2, 'movie night', '2018-12-10', '2018-12-20');
+INSERT INTO events (userid, title, from_date, to_date, invites)
+VALUES (1, 'bowling night', '12/01/2018', '12/14/2018', '["2","3"]'), 
+(2, 'game night', '1/14/2018', '1/19/2018', '["1","3"]'), 
+(2, 'movie night', '2/12/2018', '2/15/2018', '["3"]');
+
+INSERT INTO votes (userid, eventid, dates)
+VALUES (1,2, '["12","14"]');
