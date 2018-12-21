fa58e1701750890a8ff5727ce7d25ed8ec5231d0
Fife <39037648+Chris6170@users.noreply.github.com>
2018-12-20 22:06:16 -0500

    javascript_files

--- public/assets/js/login.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  handleLoginSugnup("form.login", "/api/login", "#login")
-});
-
-$(document).ready(function () {
-  handleLoginSugnup("form.signup", "/api/signup", "#signup")
-});
