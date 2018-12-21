670c66e660aecfbc78fcaec8cd24464bb160ce76
glaracuente <glaracuente@outbrain.com>
2018-12-21 15:21:24 -0500

    final final

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO events (userid, title, from_date, to_date, invites)
-VALUES (1, 'bowling night', '12/01/2018', '12/14/2018', '["2","3"]'), 
+VALUES (1, 'bowling night', '12/01/2018', '12/14/2018', '["2","3","4","5","6","7"]'), 
 (2, 'game night', '1/14/2018', '1/19/2018', '["1","3"]'), 
 (2, 'movie night', '2/12/2018', '2/15/2018', '["3"]');
 
